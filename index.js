054aef19a16c5bc878662674c2f603228ba39158
YogeshPattnaik <yogieduspace22@gmail.com>
2022-03-17 12:27:35 +0530

    initial commit for chat

---
+++ index.js
@@ -0,0 +1,51 @@
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const socketIO = require('socket.io');
+
+const app = express();
+const port = 4000 || process.env.PORT;
+
+const users = [];
+
+const server = http.createServer(app);
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Yeah its working!!');
+});
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('New Connection');
+
+  socket.on('joined', ({ user }) => {
+    users[socket.id] = user;
+    socket.broadcast.emit('userJoined', {
+      user: 'Admin',
+      message: `${users[socket.id]} has joined`,
+    });
+    socket.emit('welcome', {
+      user: 'Admin',
+      message: `Welcome to the Chat ${users[socket.id]}`,
+    });
+  });
+
+  socket.on('message', ({ message, id }) => {
+    io.emit('sendMessage', { user: users[id], message, id });
+  });
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('leave', {
+      user: 'Admin',
+      message: `${users[socket.id]} has left`,
+    });
+    console.log('User left');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`);
+});
